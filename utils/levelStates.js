bf761097ed8c90ac55a9939732095736bd6d6fa8
ababil1247 <kaziababilazamtalha@gmail.com>
2023-01-07 05:36:10 +0600

    text screens done with, level changing understood

---
+++ utils/levelStates.js
@@ -0,0 +1,29 @@
+const MENU = 0;
+const GAME_OVER = 99;
+
+// GAME_OVER -> LEVEL_X_START
+
+const GAME_CLEARED = 399;
+
+// LEVEL_X_TEXT : story dialogues
+// LEVEL_X_START : start of level, ship reset, laser array clear, asteroid array reset
+// LEVEL_X_GAME : gameplay, update, render, collision, timer, etc.
+// LEVEL_X_END : end of level, story dialogues
+
+const LEVEL_1_TEXT = 100;
+const LEVEL_1_START = 101;
+const LEVEL_1_GAME = 110;
+const LEVEL_1_END = 199;
+
+const LEVEL_2_TEXT = 200;
+const LEVEL_2_START = 201;
+const LEVEL_2_GAME = 210;
+const LEVEL_2_END = 299;
+
+const LEVEL_3_TEXT = 300;
+const LEVEL_3_START = 301;
+const LEVEL_3_GAME = 310;
+const LEVEL_3_MID = 350;
+
+
+
